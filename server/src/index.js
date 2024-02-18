6ef826877368af0d36ee5b1744020eab7443bc77
bishnuf66 <bkbishnu2000@gmail.com>
2024-02-19 00:21:11 +0545

    first upload of  working app

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://user:bBiGVhxrZvU1t3tj@cluster0.0fui2nx.mongodb.net/bhansa?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.listen(3001, () => console.log("Server started"));
